e310a1d08640647fd8321a6bc1e463c2fbceb5bf
kiolygenius <git@zhuxiaofeng.me>
2022-05-24 17:57:34 +0800

    directory structure

---
+++ src/upnp++/UpnpResourceFactory.cpp
@@ -0,0 +1,13 @@
+#include "UpnpResourceFactory.h"
+
+UpnpResourceFactory::UpnpResourceFactory()
+{
+
+}
+
+UpnpResourceFactory::~UpnpResourceFactory()
+{
+    
+}
+
+
---
+++ src/upnp++/UpnpResourceFactory.h
@@ -0,0 +1,14 @@
+#ifndef _UPNP_RESOURCE_FACTORY_
+#define _UPNP_RESOURCE_FACTORY_
+
+class UpnpResourceFactory
+{
+public:
+    UpnpResourceFactory();
+    ~UpnpResourceFactory();
+    
+private:
+    
+};
+
+#endif  //_UPNP_RESOURCE_FACTORY_
